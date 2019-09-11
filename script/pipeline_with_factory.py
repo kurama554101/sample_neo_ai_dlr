9c700fea3a6800e629968f001136be9fdf22be6e
kurama554101 <shotasakamoto554101@gmail.com>
2019-09-11 11:47:16 +0000

    tmp

---
+++ script/pipeline_with_factory.py
@@ -0,0 +1,89 @@
+import traceback
+import sys
+
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst, GObject
+
+
+# Initializes Gstreamer, it's variables, paths
+Gst.init(sys.argv)
+
+
+def on_message(bus, message, loop):
+    mtype = message.type
+    """
+        Gstreamer Message Types and how to parse
+        https://lazka.github.io/pgi-docs/Gst-1.0/flags.html#Gst.MessageType
+    """
+    if mtype == Gst.MessageType.EOS:
+        print("End of stream")
+        
+    elif mtype == Gst.MessageType.ERROR:
+        err, debug = message.parse_error()
+        print(err, debug)     
+    elif mtype == Gst.MessageType.WARNING:
+        err, debug = message.parse_warning()
+        print(err, debug)             
+        
+    return True 
+
+# Gst.Pipeline https://lazka.github.io/pgi-docs/Gst-1.0/classes/Pipeline.html
+pipeline = Gst.Pipeline()
+
+# Creates element by name
+# https://lazka.github.io/pgi-docs/Gst-1.0/classes/ElementFactory.html#Gst.ElementFactory.make
+src_name = "my_video_test_src"
+# src = Gst.ElementFactory.make("videotestsrc", "my_video_test_src")
+src = Gst.ElementFactory.make("v4l2src", "my_video_test_src")
+#src.set_property("num-buffers", 50)
+#src.set_property("pattern", "ball")
+src.set_property("device", "/dev/video0")
+src.set_property("io-mode", 4)
+pipeline.add(src)
+
+# add converter
+convert = Gst.ElementFactory.make("videoconvert")
+pipeline.add(convert)
+
+# capture
+cap1 = Gst.Caps.from_string("video/x-raw, format=NV12, width=640, height=480")
+camerafilter1 = Gst.ElementFactory.make("capsfilter")
+camerafilter1.set_property("caps", cap1)
+pipeline.add(camerafilter1)
+
+# add sink
+#sink = Gst.ElementFactory.make("gtksink")
+sink = Gst.ElementFactory.make("autovideosink")
+pipeline.add(sink)
+
+# create link
+src.link(convert)
+convert.link(camerafilter1)
+camerafilter1.link(sink)
+
+assert src == pipeline.get_by_name(src_name)
+
+# https://lazka.github.io/pgi-docs/Gst-1.0/classes/Bus.html
+bus = pipeline.get_bus() 
+
+# allow bus to emit messages to main thread 
+bus.add_signal_watch()
+
+# Add handler to specific signal
+# https://lazka.github.io/pgi-docs/GObject-2.0/classes/Object.html#GObject.Object.connect
+bus.connect("message", on_message, None)
+
+# Start pipeline
+pipeline.set_state(Gst.State.PLAYING)
+
+# Init GObject loop to handle Gstreamer Bus Events
+loop = GObject.MainLoop()
+
+try:
+    loop.run()
+except:
+    traceback.print_exc()
+    
+# Stop Pipeline
+pipeline.set_state(Gst.State.NULL)
