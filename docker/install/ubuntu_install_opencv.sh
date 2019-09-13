f1f8bd7aecd52637bc749d2b2b277d7f572546b6
kurama554101 <shotasakamoto554101@gmail.com>
2019-09-13 22:14:30 +0900

    tmp commit (modify dockerFIle)

---
+++ docker/install/ubuntu_install_opencv.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# save directory
+v_dir=`pwd`
+
+# build opencv
+ln -s /usr/include/libv4l1-videodev.h /usr/include/linux/videodev.h
+mkdir tmp
+cd tmp && wget https://github.com/Itseez/opencv/archive/3.1.0.zip && unzip 3.1.0.zip
+cd tmp/opencv-3.1.0 && cmake CMakeLists.txt -DWITH_TBB=ON \
+                                            -DINSTALL_CREATE_DISTRIB=ON \
+                                            -DWITH_FFMPEG=OFF \
+                                            -DWITH_IPP=OFF \
+                                            -DCMAKE_INSTALL_PREFIX=/usr/local
+cd tmp/opencv-3.1.0 && make -j2 && make install
+
+# clean up
+rm -rf tmp
+cd ${v_dir}
+
