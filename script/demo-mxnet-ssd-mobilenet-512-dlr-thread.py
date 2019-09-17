ad3b18bc388f9e544013cf0d4d7d91ecd7a9951c
kurama554101 <shotasakamoto554101@gmail.com>
2019-09-17 12:14:16 +0900

    tmp commit

--- script/demo-mxnet-ssd-mobilenet-512-dlr-thread.py
+++ script/demo-mxnet-ssd-mobilenet-512-dlr-thread.py
@@ -8,35 +8,14 @@ import dlr
 import threading
 
 
-
+# set parameter
 ms = lambda: int(round(time.time() * 1000))
-
 test_image = "dog.jpg"
 dshape = (1, 3, 512, 512)
 dtype = "float32"
 w = 1280
 h = 720
-result = []
-
-
-
-
-# Preprocess image
-def open_and_norm_image(frame):
-    
-    #orig_img = cv2.imread(f)
-    orig_img = frame
-    img = cv2.resize(orig_img, (dshape[2], dshape[3]))
-    img = img[:, :, (2, 1, 0)].astype(np.float32)
-    img -= np.array([123, 117, 104])
-    img = np.transpose(np.array(img), (2, 0, 1))
-    img = np.expand_dims(img, axis=0)
-    return orig_img, img
-
-
 model_path = "models/mxnet-ssd-mobilenet-512"
-
-
 class_names = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair",
                "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant",
                "sheep", "sofa", "train", "tvmonitor"]
@@ -45,10 +24,8 @@ class_names = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "
 def get_result(model, image):
     orig_img, img_data = open_and_norm_image(image)
     input_data = img_data.astype(dtype)
-
     m_out = model.run(input_data)
-    out = m_out[0][0]
-    return out
+    return m_out[0][0]
 
 
 def display(model, frame, out, thresh=0.5):
@@ -65,13 +42,22 @@ def display(model, frame, out, thresh=0.5):
         p2 = (int(right), int(bottom))
         cv2.rectangle(frame, p1, p2, (77, 255, 9), 3, 1)
         cv2.putText(frame, class_names[cid], (int(left + 10), int((top+bottom)/2)), cv2.FONT_HERSHEY_SIMPLEX,1, (255, 0, 0), 2, cv2.LINE_AA)
-        #frame = cv2.resize(frame,((int)(w*0.6),(int)(h*0.6)))
     cv2.imshow('Single-Threaded Detection',frame)
 
 
-######################################################################
-# Create TVM runtime and do inference
+# Preprocess image
+def open_and_norm_image(frame):
+    # orig_img = cv2.imread(f)
+    orig_img = frame
+    img = cv2.resize(orig_img, (dshape[2], dshape[3]))
+    img = img[:, :, (2, 1, 0)].astype(np.float32)
+    img -= np.array([123, 117, 104])
+    img = np.transpose(np.array(img), (2, 0, 1))
+    img = np.expand_dims(img, axis=0)
+    return orig_img, img
 
+
+######################################################################
 # Build TVM runtime
 device = 'opencl'
 m = dlr.DLRModel(model_path, device)
@@ -92,13 +78,9 @@ while True:
     out = get_result(m, test_image)
 
     # display image with bounding boxes
-    display(m, test_image, result, 0.5)
+    display(m, test_image, out, 0.5)
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
 
-
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
-
