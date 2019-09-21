e1fcb615bb1018aae988af1dc6c1752aba6d8c1e
kurama554101 <shotasakamoto554101@gmail.com>
2019-09-21 20:01:22 +0900

    tmp commit

---
+++ script/neo_wrapper.py
@@ -0,0 +1,20 @@
+
+
+class SageMakerNeoWrapper:
+    def __init__(self):
+        # TODO : imp
+        pass
+
+    def load(self):
+        # TODO : imp
+        pass
+
+    def run(self, img):
+        # TODO : imp
+        pass
+
+
+class NeoInferResult:
+    def __init__(self):
+        # TODO : imp
+        pass
