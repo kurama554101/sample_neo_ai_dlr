ce2a601bff57325dd31674d77c3d4e3e4c513816
kurama554101 <shotasakamoto554101@gmail.com>
2019-09-24 00:49:40 +0900

    tmp commit

--- script/sample_infer.py
+++ script/sample_infer.py
@@ -19,12 +19,12 @@ def main():
 
     # read image
     file_path = "data/dog.jpg"
-    im = cv2.imread(file_path)
+    im = cv2.imread(file_path)  # shape is (H, W, C)
     cv2_images = [im]
     file_path_list = [file_path]
 
     # run inference
-    size = (768, 576)
+    size = (im.shape[1], im.shape[0])
     result = wrapper.run(original_images=cv2_images, output_size=size, file_name_list=file_path_list)
     print(result)
 
