fa6c0493dc8f5823b2a682b6df2e0e12e3c85a4b
kurama554101 <shotasakamoto554101@gmail.com>
2019-10-08 02:25:39 +0900

    [Mod] add sidebar to modify parameter of face recognition

--- script/run_face_recognition.py
+++ script/run_face_recognition.py
@@ -1,39 +1,11 @@
 import time
-import argparse
 from multiprocessing import set_start_method
 import platform
 from face_recognition_util import FaceRecognitionMode, VideoCaptureParams, RealTimeFaceRecognition, DisplayType
 import streamlit as st
 
 
-def create_argument_parser():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument(
-        "--capture_size",
-        default="vga",
-        help="set capture size. you can select it from 'vga', '720p', '1080p'. default size is 'vga'"
-    )
-    parser.add_argument(
-        "--capture_fps",
-        default=60,
-        type=int
-    )
-    parser.add_argument(
-        "--reduction_ratio",
-        default=4,
-        type=int
-    )
-    parser.add_argument(
-        "--count_of_using_frame",
-        default=50,
-        type=int
-    )
-    return parser
-
-
-def get_capture_size(args):
-    str_arg = args.capture_size
+def get_capture_size(str_arg):
     if str_arg == "vga":
         return DisplayType.VGA.value
     elif str_arg == "720p":
@@ -42,28 +14,6 @@ def get_capture_size(args):
         return DisplayType.HDTV1080p.value
     else:
         raise Exception("{} capture size is not defined!".format(str_arg))
-
-
-def get_mode(args):
-    str_arg = args.mode
-    if str_arg == "one_face_recognition":
-        return FaceRecognitionMode.OneFaceRecognitionMode
-    elif str_arg == "draw_bounding_box":
-        return FaceRecognitionMode.DrawBoundingBoxMode
-    else:
-        raise Exception("{} mode is not defined!".format(str_arg))
-
-
-def get_fps(args):
-    return args.capture_fps
-
-
-def get_reduction_ratio(args):
-    return args.reduction_ratio
-
-
-def get_use_frame_count(args):
-    return args.count_of_using_frame
 
 
 @st.cache(ignore_hash=True)
@@ -78,7 +28,8 @@ def get_recognition_module(param, frame_count_with_use_face_recog, reduction_rat
     return recognition
 
 
-if __name__ == "__main__":
+@st.cache(ignore_hash=True)
+def set_server_method():
     # fix bug of macOS
     if platform.system() == 'Darwin':
         try:
@@ -87,17 +38,20 @@ if __name__ == "__main__":
             print("RuntimeError: context has already been set")
             pass
 
-    # get parser and parse argument of command line
-    parser = create_argument_parser()
-    args = parser.parse_args()
-    capture_size = get_capture_size(args)
-    capture_fps = get_fps(args)
-    frame_count_with_use_face_recog = get_use_frame_count(args)
-    reduction_ratio = get_reduction_ratio(args)
 
+if __name__ == "__main__":
+    set_server_method()
+
+    # set sidebar
+    st.sidebar.title("setup parameter")
+    fps = st.sidebar.slider("fps", min_value=10, max_value=60, value=30)
+    display_mode_str = st.sidebar.selectbox("display mode", ("vga", "720p", "1080p"))
+    frame_count_with_use_face_recog = st.sidebar.slider("frame count with use face recognition", min_value=2,
+                                                        max_value=50, value=10)
+    reduction_ratio = st.sidebar.selectbox("reduction ratio", (4, 2))
     param = VideoCaptureParams()
-    param.size = capture_size
-    param.fps = capture_fps
+    param.size = get_capture_size(display_mode_str)
+    param.fps = fps
 
     # setup
     print("start to setup...")
@@ -120,7 +74,7 @@ if __name__ == "__main__":
     st.image(result.FrameData)
 
     # set reload button
-    st.button("reload")
+    st.sidebar.button("reload")
 
     # end
     print("end")
--- script/sample_streamlit.py
+++ script/sample_streamlit.py
@@ -2,29 +2,46 @@ import streamlit as st
 from face_recognition_util import RealTimeFaceRecognition, VideoCaptureParams, DisplayType, FaceRecognitionMode
 from multiprocessing import set_start_method
 import platform
+import time
 
 
 def main():
+    # setup bar
+    st.sidebar.title("setup parameter")
+    fps = st.sidebar.slider("fps", min_value=10, max_value=60, value=30)
+    display_mode_str = st.sidebar.selectbox("display mode", ("vga", "720p", "1080p"))
+    frame_count_with_use_face_recog = st.sidebar.slider("frame count with use face recognition", min_value=2, max_value=50, value=10)
+    reduction_ratio = st.sidebar.selectbox("reduction ratio", (4, 2))
     param = VideoCaptureParams()
-    param.size = DisplayType.VGA.value
-    param.fps = 30
-    frame_count_with_use_face_recog = 10
-    reduction_ratio = 4
+    param.size = get_capture_size(display_mode_str)
+    param.fps = fps
 
     with st.spinner('Wait for setup...'):
         rtfr = get_recognition_module(param, frame_count_with_use_face_recog, reduction_ratio)
     st.success('setup done!')
 
     with st.spinner("Wait for run..."):
-        rtfr.run()
+        # rtfr.run()
+        time.sleep(1)
     st.success('run done!')
 
     # write frame of face recognition
-    result = rtfr.get_result()
-    st.image(result.FrameData)
+    # result = rtfr.get_result()
+    # st.image(result.FrameData)
 
     # reload button
-    st.button("reload")
+    st.sidebar.button("reload")
+
+
+def get_capture_size(str_arg):
+    if str_arg == "vga":
+        return DisplayType.VGA.value
+    elif str_arg == "720p":
+        return DisplayType.HDTV720p.value
+    elif str_arg == "1080p":
+        return DisplayType.HDTV1080p.value
+    else:
+        raise Exception("{} capture size is not defined!".format(str_arg))
 
 
 @st.cache(ignore_hash=True)
@@ -40,7 +57,8 @@ def get_recognition_module(param, frame_count_with_use_face_recog, reduction_rat
     return rtfr
 
 
-if __name__ == "__main__":
+@st.cache(ignore_hash=True)
+def set_server_method():
     # fix bug of macOS
     if platform.system() == 'Darwin':
         try:
@@ -49,4 +67,7 @@ if __name__ == "__main__":
             print("RuntimeError: context has already been set")
             pass
 
+
+if __name__ == "__main__":
+    set_server_method()
     main()
